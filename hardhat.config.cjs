6c93dc557cf35b194f0545a946b02cf3c4f99343
Mansi Parmar <mansiparmar663@gmail.com>
2025-08-20 20:31:11 +0530

    Deployed contract to Sepolia and verified on Etherscan

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,14 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
-    localhost: {
-      url: "http://127.0.0.1:8545",  // local Hardhat node
-    },
     sepolia: {
-      url: "https://eth-sepolia.g.alchemy.com/v2/qv1oHmXQDMgUAXQ1llBuM", // replace with your HTTPS RPC URL
-      accounts: ["0135f055c86dd3955e77e356ee26b6ec10fd6cba4b523157f18203a32d2f0a6e"] // replace with your MetaMask exported private key
+      url: process.env.SEPOLIA_RPC_URL,        // Reads Alchemy/Infura URL from .env
+      accounts: [process.env.PRIVATE_KEY]      // Reads your MetaMask private key from .env
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY       // Reads Etherscan API key from .env
   }
 };
